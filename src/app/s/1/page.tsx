94a24cc8832cc3250fbab286fe77187f6d091ed7
simosabba <simone.sabba@live.com>
2025-12-10 23:06:11 +0100

    feat: update Slide1 component with new titles for improved introduction

--- src/app/s/1/page.tsx
+++ src/app/s/1/page.tsx
@@ -4,8 +4,9 @@ export default function Slide1() {
   return (
     <SlideContainer slideNumber={1}>
       <div className="flex h-full items-start gap-24 pt-32">
-        <div className="flex-shrink-0">
-          <SlideTitle>Chi Sono</SlideTitle>
+        <div className="flex flex-col gap-[2px]">
+          <SlideTitle>Ciao,</SlideTitle>
+          <SlideTitle>sono Simo</SlideTitle>
         </div>
 
         <div className="slide-content max-w-xl">
